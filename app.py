1294fadaf708c65b2908e5363abe6001a59c012f
shreysahgal <shreysahgal@gmail.com>
2020-05-19 03:46:01 -0400

    made it so that the same photo can't be randomly picked twice

--- app.py
+++ app.py
@@ -6,16 +6,21 @@ app = Flask(__name__)
 app.secret_key = b'%\x14BNS\x8b\xcd\xbe\x11\x1b%`\xe0x\xe7\xc5'  # sshhhh!
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
 	d_f = ['Dancing', 'Falling'][randint(0,1)] # dancing or falling?
-	i = randint(1, 9)
+	if request.method=='GET':
+		i = randint(1, 9)
+	else:
+		j = request.form['prev']
+		i = [k for k in range(1,9) if k != j][randint(0,7)]
+
 	imgpath = 'pics/%s/%d/%d.PNG' % (d_f, i, i)
 	print(imgpath)
 	if d_f == 'Dancing':
-		return render_template('dancing.html', img=imgpath)
+		return render_template('dancing.html', img=imgpath, index=i)
 	else:
-		return render_template('falling.html', img=imgpath)
+		return render_template('falling.html', img=imgpath, index=i)
 
 
 @app.route('/wrong')
